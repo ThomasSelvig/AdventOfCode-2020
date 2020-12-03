a8d25bd852285ccd4fdc8ad5822a6cfd7354271e
ThomasSelvig <41569255+ThomasSelvig@users.noreply.github.com>
2020-12-03 09:15:39 +0100

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,34 @@
+import os
+
+
+assert "day3.txt" in os.listdir(), "Paste puzzle input in 'day3.txt'"
+
+with open("day3.txt") as f:
+	sledmap = [i.strip() for i in f.readlines()]
+
+
+def drive_sled(right, down):
+	x = 0
+	crash_count = 0
+	for line in sledmap[::down]:
+		if line[x % len(line)] == "#":
+			crash_count += 1
+		x += right
+	
+	return crash_count
+
+
+print("First:", drive_sled(3, 1))
+
+combos = [
+	(1, 1),
+	(3, 1),
+	(5, 1),
+	(7, 1),
+	(1, 2)
+]
+
+ans = 1
+for crash_sum in [drive_sled(r, d) for r, d in combos]:
+	ans *= crash_sum
+print("Second:", ans)
